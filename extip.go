885cd8206e793f153c6e6d7a048a69a61eaca78f
skubota <skubota@xn--y8jmg4a9b7g.jp>
2020-08-07 09:45:03 +0900

    update

--- extip.go
+++ extip.go
@@ -31,6 +31,28 @@ func GetIP() (net.IP, error) {
         return xorAddr.IP, nil
 }
 
+// GetPort is get external source port
+func GetPort() (int, error) {
+        c, err := stun.Dial("udp", stunsrv)
+        var xorAddr stun.XORMappedAddress
+        if err != nil {
+                // no route to host
+                return -1, nil
+        }
+        message := stun.MustBuild(stun.TransactionID, stun.BindingRequest)
+        if err := c.Do(message, func(res stun.Event) {
+                if res.Error != nil {
+                        err = res.Error
+                }
+                if err := xorAddr.GetFrom(res.Message); err != nil {
+                        return
+                }
+        }); err != nil {
+                return -1, err
+        }
+        return xorAddr.Port, nil
+}
+
 // GetIP4 is get external ipv4 address
 func GetIP4() (net.IP, error) {
 	c, err := stun.Dial("udp4", stunsrv)
@@ -53,6 +75,28 @@ func GetIP4() (net.IP, error) {
 	return xorAddr.IP, nil
 }
 
+// GetPort4 is get external source port
+func GetPort4() (int, error) {
+        c, err := stun.Dial("udp4", stunsrv)
+        var xorAddr stun.XORMappedAddress
+        if err != nil {
+                // no route to host
+                return -1, nil
+        }
+        message := stun.MustBuild(stun.TransactionID, stun.BindingRequest)
+        if err := c.Do(message, func(res stun.Event) {
+                if res.Error != nil {
+                        err = res.Error
+                }
+                if err := xorAddr.GetFrom(res.Message); err != nil {
+                        return
+                }
+        }); err != nil {
+                return -1, err
+        }
+        return xorAddr.Port, nil
+}
+
 // GetIP6 is get external ipv6 address
 func GetIP6() (net.IP, error) {
 	c, err := stun.Dial("udp6", stunsrv)
@@ -73,4 +117,26 @@ func GetIP6() (net.IP, error) {
 		return net.IP(nil), err
 	}
 	return xorAddr.IP, nil
+}
+
+// GetPort6 is get external source port
+func GetPort6() (int, error) {
+        c, err := stun.Dial("udp6", stunsrv)
+        var xorAddr stun.XORMappedAddress
+        if err != nil {
+                // no route to host
+                return -1, nil
+        }
+        message := stun.MustBuild(stun.TransactionID, stun.BindingRequest)
+        if err := c.Do(message, func(res stun.Event) {
+                if res.Error != nil {
+                        err = res.Error
+                }
+                if err := xorAddr.GetFrom(res.Message); err != nil {
+                        return
+                }
+        }); err != nil {
+                return -1, err
+        }
+        return xorAddr.Port, nil
 }
--- extip_test.go
+++ extip_test.go
@@ -7,24 +7,48 @@ import (
 
 func TestGetIP(t *testing.T) {
         address, err := GetIP()
-        fmt.Printf("GetIP: %s\n", address)
         if err != nil {
                 t.Errorf("Error: %s", err)
         }
+        fmt.Printf("GetIP: %s\n", address)
+}
+
+func TestGetPort(t *testing.T) {
+        port, err := GetPort()
+        if err != nil {
+                t.Errorf("Error: %s", err)
+        }
+        fmt.Printf("GetPort: %d\n", port)
 }
 
 func TestGetIP4(t *testing.T) {
 	address, err := GetIP4()
-	fmt.Printf("GetIP4: %s\n", address)
 	if err != nil {
 		t.Errorf("Error: %s", err)
 	}
+	fmt.Printf("GetIP4: %s\n", address)
+}
+
+func TestGetPort4(t *testing.T) {
+        port4, err := GetPort4()
+        if err != nil {
+                t.Errorf("Error: %s", err)
+        }
+        fmt.Printf("GetPort4: %d\n", port4)
 }
 
 func TestGetIP6(t *testing.T) {
 	address, err := GetIP6()
-	fmt.Printf("GetIP6: %s\n", address)
 	if err != nil {
 		t.Errorf("Error: %s", err)
 	}
+	fmt.Printf("GetIP6: %s\n", address)
+}
+
+func TestGetPort6(t *testing.T) {
+        port6, err := GetPort6()
+        if err != nil {
+                t.Errorf("Error: %s", err)
+        }
+        fmt.Printf("GetPort6: %d\n", port6)
 }
